19b0a3da7c673918032028b3d21cce0bf850c9a8
Nicolas <s2518809@ed.ac.uk>
2025-01-24 19:36:05 +0000

    update datasaving

--- model/save_simulation.py
+++ model/save_simulation.py
@@ -38,12 +38,12 @@ liberal_betas = mf_priors[mf_priors.bin_pol=='liberal'].groupby('id').apply(
 
 
 beta = True # Use beta distributions for moral foundations
-n_sims = 10
-n_agent_mean = 10 # mean of normal 
-n_agent_scale = 5 # std dev
+n_sims = 5
+n_agent_mean = 100 # mean of normal 
+n_agent_scale = 15 # std dev
 
 n_agent_samples = np.random.normal(loc=n_agent_mean, scale=n_agent_scale,size=n_sims)
-n_steps = 100
+n_steps = 1501
 
 data_loc = './simulation_data/'
 
@@ -63,18 +63,23 @@ for sim in range(n_sims):
     abm = MoralABM(n_agents = n_agents,
                    n_steps = n_steps,
                    priors = [conservative_priors,liberal_priors],
-                   beta_prior=True)
-    
-    AMs = np.array([abm.get_adjacency_matrix(step) for step in range(n_steps) if step%10 ==0])
-    Belief_AMs = np.array([abm.get_adjacency_matrix(step,belief=True) for step in range(n_steps) if step%10 ==0])
+                   beta_prior=beta)
 
     # save the data    
+    np.save(data_loc+'mf_'+f'{'beta' if beta else 'dirichlet'}'+'-' + str(sim_id) + '.npy',
+            abm.M_agents[:,:,:-1,[n%10==0 for n in range(n_steps)]])
+
     pd.DataFrame(
-        [{mf: abm.M_agents[i,i,mfi,t] for mfi,mf in enumerate(foundations)}|{'id': abm.agent_ids[i]}|{'step':t}|{'bin_pol': ['conservative','liberal'][int(i >= n_agents//2)]} 
+        [{mf + '_a': abm.M_agents[i,i,2*mfi,t] for mfi,mf in enumerate(foundations)}| {mf + '_b': abm.M_agents[i,i,2*mfi+1,t] for mfi,mf in enumerate(foundations)} | {'id': abm.agent_ids[i]}|{'step':t}|{'bin_pol':['conservative','liberal'][int(i >= n_agents//2)]}
          for i in range(n_agents) for t in range(n_steps) if t%10==0]
-    ).to_csv(data_loc+'moral_abm_beta-'+str(sim_id)+'.csv',
+    ).to_csv(data_loc+'moral_abm_'+f'{'beta' if beta else 'dirichlet'}'+'-'+str(sim_id)+'.csv',
             index=False)
     
-    np.save(data_loc+'mf_graphs_beta-' + str(sim_id) + '.npy', AMs)
-    np.save(data_loc+'belief_graphs_beta-' + str(sim_id) + '.npy', Belief_AMs)
+
+    # AMs = np.array([abm.get_adjacency_matrix(step) for step in range(n_steps) if step%10 ==0])
+    # Belief_AMs = np.array([abm.get_adjacency_matrix(step,belief=True) for step in range(n_steps) if step%10 ==0])
+
+    
+    # np.save(data_loc+'mf_graphs_beta-' + str(sim_id) + '.npy', AMs)
+    # np.save(data_loc+'belief_graphs_beta-' + str(sim_id) + '.npy', Belief_AMs)
 
